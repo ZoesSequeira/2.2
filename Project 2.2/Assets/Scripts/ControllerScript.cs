3b4dd7a2155b4d9c37118102e1ce7f0d4947f984
zcs18 <zcs18@hampshire.edu>
2019-10-28 00:02:23 -0400

    no message

---
+++ Project 2.2/Assets/Scripts/ControllerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerScript : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    Vector3 cubePosition;
+    public static GameObject airplane;
+    public static GameObject activeAirplane;
+    public static GameObject[,] cubeGrid;
+    int gridX = 16;
+    int gridY = 9;
+    int airplaneX = 0;
+    int airplaneY = 8;
+
+    // Start is called before the first frame update
+   
+    void Start()
+    {
+        cubeGrid = new GameObject[gridX, gridY];
+        for (int y = 0; y <gridY; y ++)
+        {
+            for (int x = 0; x < gridX; x++)
+            {
+                cubePosition = new Vector3(x*2, y*2, 0);
+                cubeGrid[x,y] = Instantiate(cubePrefab, cubePosition, Quaternion.identity);
+            }
+        }
+        cubeGrid[airplaneX, airplaneY].GetComponent<Renderer>().material.color = Color.red;
+        airplane = cubeGrid[airplaneX, airplaneY];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Project 2.2/Assets/Scripts/CubeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        if (ControllerScript.airplane == gameObject)
+        {
+            ControllerScript.airplane.transform.localScale = new Vector3 (1.2f, 1.2f, 1.2f);
+            ControllerScript.activeAirplane = gameObject;
+            ControllerScript.airplane = null;
+        }
+        else if (ControllerScript.activeAirplane == gameObject)
+        {
+            ControllerScript.activeAirplane.transform.localScale = new Vector3(1f, 1f, 1f);
+            ControllerScript.airplane = gameObject;
+            ControllerScript.activeAirplane = null;
+        }
+        else if (ControllerScript.activeAirplane != null)
+            {
+                ControllerScript.activeAirplane.GetComponent<Renderer>().material.color = Color.white; //turns last cube white
+                ControllerScript.activeAirplane.transform.localScale = new Vector3(1f, 1f,1f);
+                ControllerScript.activeAirplane = gameObject;//sets currently clicked cube to active
+                ControllerScript.activeAirplane.GetComponent<Renderer>().material.color = Color.red; 
+                ControllerScript.activeAirplane.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+        }
+
+
+    }
+}
